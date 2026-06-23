fec7f8d4f40c74c6c93590b2359f415075fe444e
breaksek <98497506+breaksek@users.noreply.github.com>
2026-06-23 08:36:49 +0700

    Update Main.java

--- uas/Main.java
+++ uas/Main.java
@@ -7,6 +7,7 @@ public class Main {
 
     public static void main(String[] args) {
         int pilih;
+
         do {
             System.out.println("\n===== MENU =====");
             System.out.println("1. Tampilkan Data");
