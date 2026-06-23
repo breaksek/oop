ec894d7a99bb4362e53e011da9aebc233765ab87
breaksek <98497506+breaksek@users.noreply.github.com>
2026-06-23 08:05:07 +0700

    Create Orang.java

---
+++ uas/Orang.java
@@ -0,0 +1,19 @@
+public abstract class Orang {
+    private String nama;
+    private String alamat;
+
+    public Orang(String nama, String alamat) {
+        this.nama = nama;
+        this.alamat = alamat;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+
+    public abstract void tampilInfo();
+}
